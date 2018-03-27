545e33484e257790059e06d7dc9c4637a42065dc
garricknorthover <garricknorthover@gmail.com>
2018-03-28 09:21:51 +1300

    2nd test fails due to spaces

--- cipher.rb
+++ cipher.rb
@@ -1,22 +1,33 @@
 
 CONSTANT = 'abcdefghijklmnopqrstuvwxyz'
-# probably should split this up a little
+
+# Splits encrypted word into an array and then mapped over to find the value for the key
 def decrypt(word, cipher)
-  result = Hash[cipher.split("").zip(CONSTANT.split(""))]
-  word.split("").map { |x| result[x] }.join()
+  word.split("").map { |x| array_to_hash(cipher)[x] }.join()
+end
+
+# Takes the 2 arrays and combines them ta hash with key value pairs
+def array_to_hash(cipher)
+  Hash[cipher.split("").zip(CONSTANT.split(""))]
 end
 
 cipher = 'zodvqukgwefbyitmrsplhacxnj'
 puts decrypt('dzs', cipher)
-#just one test so far
 
+# TESTS!
 
+describe "test the  cipher" do
+  cipher = 'zodvqukgwefbyitmrsplhacxnj'
+  it "car decrypted" do
+    expect(decrypt("dzs", cipher)).to eq 'car'
+  end
+end
 
-# describe "test the  cipher" do
-#   cipher = 'zodvqukgwefbyitmrsplhacxnj'
-#   it "car decrypted" do
-#     expect(decrypt("dzs", cipher)).to eq 'dzs'
-#   end
-# end
+describe "2nd example" do
+  cipher = 'xipmuzfkbrlwotjancqgveshdy'
+  it "2nd example decrypted" do
+    expect(decrypt("skd qj qucbjvq?", cipher)).to eq 'why so serious?'
+  end
+end
 
 # note to remember Any character other than what you have in the constant doesn't have to be replaced.
