4c9430f1cc4d87dba998e06ea2991b029d755cdc
garricknorthover <garricknorthover@gmail.com>
2018-03-28 08:45:17 +1300

    Basic decrypt works

--- cipher.rb
+++ cipher.rb
@@ -1,19 +1,22 @@
 
+CONSTANT = 'abcdefghijklmnopqrstuvwxyz'
 
-class Atbash
-  def encrypt(word)
-    if word == 'car'
-      'dzs'
-    end
-  end
+def decrypt(word, cipher)
+  result = Hash[cipher.split("").zip(CONSTANT.split(""))]
+  word.split("").map { |x| result[x] }.join()
 end
-# just one test so far
-#
-describe "test the  cipher" do
-  CONSTANT = 'abcdefghijklmnopqrstuvwxyz'
-  cipher = 'zodvqukgwefbyitmrsplhacxnj'
-  at = Atbash.new
-  it "car encrypted" do
-    expect(at.encrypt("car")).to eq 'dzs'
-  end
-end
+
+cipher = 'zodvqukgwefbyitmrsplhacxnj'
+puts decrypt('dzs', cipher)
+#just one test so far
+
+
+
+# describe "test the  cipher" do
+#   cipher = 'zodvqukgwefbyitmrsplhacxnj'
+#   it "car decrypted" do
+#     expect(decrypt("car", cipher)).to eq 'dzs'
+#   end
+# end
+
+# note to remember Any character other than what you have in the constant doesn't have to be replaced.
